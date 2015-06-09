c2417326712381ee7e091d48dfffe5e8ce79efdb
aslimaye90 <aslimaye90@gmail.com>
2015-06-09 08:19:18 -0700

    added 3 controllers

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,12 +3,27 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(function ($routeProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: 'partials/def'
+		templateUrl: 'partials/def',
+		controller: 'ctrlDef'
 	})
 	.when('/partials/p1', {
-  		templateUrl: 'partials/1'
+  		templateUrl: 'partials/1',
+  		controller: 'ctrl1'
 	})
 	.when('/partials/p2', {
-	  templateUrl: 'partials/2'
+	  templateUrl: 'partials/2',
+	  controller: 'ctrl2'
 	})
+});
+
+app.controller('ctrl1', function($scope){
+	$scope.greeting = "Hello world!";
+});
+
+app.controller('ctrl2', function($scope){
+	$scope.greeting = "Hi world!";
+});
+
+app.controller('ctrlDef', function($scope){
+	$scope.greeting = "Default - Hello world!";
 });
